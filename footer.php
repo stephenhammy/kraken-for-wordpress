0190b60f12455adab047eff903c9b253117d6611
Chris Ferdinandi <hello@gomakethings.com>
2013-10-17 14:43:39 -0400

    Internationalized

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
-			<footer class="text-center text-small">
-				<hr>
+			<footer>
 
-				<p class="space-bottom-small"><a href="<?php echo site_url(); ?>/search/"><i class="icon-search"></i> Search</a> &nbsp;&bull;&nbsp; <a href="http://feeds.feedburner.com/GoMakeThings"><i class="icon-rss"></i> RSS Feed</a></p>
+				<p><?php echo sprintf( __( 'Copyright &copy; %1$s %2$s. All rights reserved.', 'kraken' ), date( 'Y' ), get_bloginfo( 'name' ) ); ?></p>
 
-				<p class="space-bottom">Made <span class="show-font-face">with <i class="icon-heart"></i><span class="screen-reader">love</span></span> by Chris Ferdinandi.</p>
 			</footer>
 
 		</section>
